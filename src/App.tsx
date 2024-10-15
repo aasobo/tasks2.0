7ae03d6c0b8ce9533bb121a8f5de2ade7739945f
aasobo <aasobo@udel.com>
2024-10-15 19:41:27 -0400

    added photo, columns, and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-//import myImage from "/Users/angelasobo/Desktop/tasks/src/assets/abozjwk9aj7d1.jpeg";       <img src={myImage} alt="2024 Boston Celtics" />;
+import myImage from "/Users/angelasobo/Desktop/tasks2.0/src/celtics.jpeg";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -10,11 +11,54 @@ function App(): React.JSX.Element {
                 Hello World! ANGEL ASOBO UD CISC275 with React Hooks and
                 TypeScript
             </header>
+            <img src={myImage} alt="2024 Boston Celtics" />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. \
             </p>
+            {/* Added List */}
+            <ul>
+                <li>First game</li>
+                <li>Second game</li>
+                <li>Third game</li>
+            </ul>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        >
+                            Column 1
+                        </div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        >
+                            Column 2
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
+
+        //Angel Asobo
     );
 }
 
