c03776e7027153544c0a5fa35fc2d4dffe47ea35
aasobo <aasobo@udel.com>
2024-10-15 18:47:36 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+//import myImage from "/Users/angelasobo/Desktop/tasks/src/assets/abozjwk9aj7d1.jpeg";       <img src={myImage} alt="2024 Boston Celtics" />;
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                <h1>Welcome to My Application</h1>
+                HELLO WORLD! ANGEL ASOBO UD CISC275 with React Hooks and
+                TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
